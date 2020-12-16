d2b898de5333d67eb32a1f302319d211962dbe42
Camilla Näsström <camilla.ljudprod@gmail.com>
2020-12-16 11:58:27 +0100

    First commit

---
+++ hello-world-git/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  hello-world-git
+//
+//  Created by Lisa Camilla Näsström on 2020-12-16.
+//
+
+#include <iostream>
+
+int main() {
+    
+    std::cout << "Hello, World!\n";
+    
+    return 0;
+    
+}
